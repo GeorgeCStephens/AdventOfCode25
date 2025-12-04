54b7bf8e8ff266d2b82f59a190087db06c48e29e
GeorgeCStephens <dga2601@gmail.com>
2025-12-04 10:45:48 +0000

    Day 2. Part 1. Complete

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,10 +2,11 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
         //Set total count of dupes seen
-        //TODO
+        let mut count: i64 = 0;
 
         //Read in the input file into a single string
-        let message: String = fs::read_to_string("../input.txt")?;
+        let mut message: String = fs::read_to_string("../input.txt")?;
+        message = message.trim().to_string();
 
         //Split the file up by ,
         let file_splitter = message.split(",");
@@ -20,17 +21,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             
             //Store the bottom and top range
             let bottom_range: i64 = both_ranges[0].parse().unwrap();
-            let top_range: i64 = both_ranges[1].parse().unwrap();
+            let mut top_range: i64 = both_ranges[1].parse().unwrap();
+            top_range = top_range + 1;
+
 
             //Loop through the range
             for check_number in bottom_range..top_range {
-                //Check for dupe
-                //Increment dupe seen counter
-                //TODO
+                let string_number: String = check_number.to_string();
 
-                println!("{}", check_number);
+                //Split the number in half
+                let (left_half, right_half) = string_number.split_at(string_number.len()/2); 
+
+                //Check for dupe
+                if left_half == right_half {
+                    //Increment dupe seen counter
+                    println!("{}", check_number);
+                    count = count + check_number;
+                }
             }
 
-        } 
+        }
+        println!("{}", count);
         Ok(())
 }
