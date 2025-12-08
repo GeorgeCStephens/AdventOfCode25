f4cb5a73c1e5fc46e8f340ab9fe243b38f537933
georgecstephens <dga2601@gmail.com>
2025-12-08 19:22:07 +0000

    Progress made on Day3 Part1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -12,6 +12,32 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn main() {
+fn get_largest_battery_pos(bank: Vec<char>) -> i32 {
+    let mut index: i32 = 0;
+    let mut largest_seen_num : i32 = 0;
+    let max_possible_num : i32 = 9;
 
+    for battery in bank {
+        let num_to_check: i32 = battery as i32 - 0x30;
+
+        if num_to_check == max_possible_num {
+            return index;
+        }
+
+        if num_to_check > largest_seen_num {
+            largest_seen_num = num_to_check;
+        }
+
+        index = index + 1;
+    }
+    return index;
+}
+
+fn main() {
+    let lines = lines_from_file("../input.txt");
+    for line in lines {
+        let bank:Vec<char> = line.chars().collect();
+        let largest_battery_pos: i32 = get_largest_battery_pos(bank);
+        println!("{} : {}", bank[largest_battery_pos], line);
+    }
 }
