2afc508aa39beb61c34e054d49bbb89f600fff2c
GeorgeCStephens <dga2601@gmail.com>
2025-12-09 17:47:09 +0000

    Day 3 Part 1 rust attempt

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -2,6 +2,7 @@ use std::{
     fs::File,
     io::{prelude::*, BufReader},
     path::Path,
+    convert::TryInto,
 };
 
 fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
@@ -12,32 +13,46 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn get_largest_battery_pos(bank: Vec<char>) -> i32 {
-    let mut index: i32 = 0;
-    let mut largest_seen_num : i32 = 0;
-    let max_possible_num : i32 = 9;
+fn get_largest_battery_pos(bank: String) -> u32 {
+    let mut index_of_largest_num: u32 = 0;
+    let mut largest_seen_num: char = '0';
 
-    for battery in bank {
-        let num_to_check: i32 = battery as i32 - 0x30;
+    for i in 0..bank.len() {
+        let num_to_check: char = bank.chars().nth(i).unwrap();
 
-        if num_to_check == max_possible_num {
-            return index;
+        if num_to_check == '9' {
+            return i as u32;
         }
 
         if num_to_check > largest_seen_num {
+            index_of_largest_num = i as u32;
             largest_seen_num = num_to_check;
         }
-
-        index = index + 1;
     }
-    return index;
+    return index_of_largest_num;
 }
 
 fn main() {
     let lines = lines_from_file("../input.txt");
+    let mut count: u64 = 0;
     for line in lines {
-        let bank:Vec<char> = line.chars().collect();
-        let largest_battery_pos: i32 = get_largest_battery_pos(bank);
-        println!("{} : {}", bank[largest_battery_pos], line);
+        // Get tenth digit
+        let first_line = &line[..line.len()-2];
+        let largest_battery_pos: u32 = get_largest_battery_pos(first_line.to_string());
+        let tenth_digit: char = line.chars().nth(largest_battery_pos.try_into().unwrap()).unwrap();
+
+        // Get oneth digit
+        let sub_line = &line[largest_battery_pos.try_into().unwrap()..];
+        let largest_battery_pos: u32 = get_largest_battery_pos(sub_line.to_string());
+        let oneth_digit: char = line.chars().nth(largest_battery_pos.try_into().unwrap()).unwrap();
+
+        // Get the largest num
+        let mut largest_num: String = "".to_string();
+        largest_num.push(tenth_digit);
+        largest_num.push(oneth_digit);
+
+        // Add em up
+        count = count + (largest_num.parse::<u64>().unwrap());
     }
+    println!("{}", count);
 }
