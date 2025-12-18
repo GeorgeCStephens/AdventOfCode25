dbe779a37be7548786e4136144a853258f092168
GeorgeCStephens <dga2601@gmail.com>
2025-12-18 17:53:01 +0000

    Day 4 setup

--- java/src/main/java/uk/co/georgestephens/days/Day3.java
+++ java/src/main/java/uk/co/georgestephens/days/Day3.java
@@ -1,16 +1,10 @@
 package uk.co.georgestephens.days;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import uk.co.georgestephens.util.InputReader;
-
 public class Day3 {
+	final String RESOURCES = "C:\\Users\\dga26\\aoc\\java\\src\\main\\resources\\Days\\";
 
-	public void run() throws IOException {
-		//TODO 5 Solve Day3
-		String fileContent = InputReader.getFileAsString("C:\\Users\\dga26\\aoc\\java\\src\\main\\resources\\Days\\Day3.txt");
-		ArrayList<String> fileContent2 = InputReader.getFileAsArray("C:\\Users\\dga26\\aoc\\java\\src\\main\\resources\\Days\\Day3.txt");
+	public void run() {
+
 	}
 
 }
---
+++ java/src/main/java/uk/co/georgestephens/days/Day4.java
@@ -0,0 +1,18 @@
+package uk.co.georgestephens.days;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import uk.co.georgestephens.util.InputReader;
+
+public class Day4 {
+	final static String RESOURCES = "C:\\Users\\dga26\\aoc\\java\\src\\main\\resources\\Days\\";
+
+	public static void main() throws IOException {
+		//TODO 5 Solve Day3
+		ArrayList<String> fileContent = InputReader.getFileAsArray(RESOURCES+"Day4.txt");
+		System.out.println(fileContent);
+		
+	}
+
+}
