c2ad713544b877c4d9a691f3d5202c07d0f06c29
GeorgeCStephens <dga2601@gmail.com>
2025-12-18 14:57:51 +0000

    Created a utility method to read a files lines into an Array

--- java/src/main/java/uk/co/georgestephens/days/Day3.java
+++ java/src/main/java/uk/co/georgestephens/days/Day3.java
@@ -1,6 +1,7 @@
 package uk.co.georgestephens.days;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import uk.co.georgestephens.util.InputReader;
 
@@ -9,7 +10,7 @@ public class Day3 {
 	public void run() throws IOException {
 		//TODO 5 Solve Day3
 		String fileContent = InputReader.getFileAsString("C:\\Users\\dga26\\aoc\\java\\src\\main\\resources\\Days\\Day3.txt");
-		System.out.println(fileContent);
+		ArrayList<String> fileContent2 = InputReader.getFileAsArray("C:\\Users\\dga26\\aoc\\java\\src\\main\\resources\\Days\\Day3.txt");
 	}
 
 }
--- java/src/main/java/uk/co/georgestephens/util/InputReader.java
+++ java/src/main/java/uk/co/georgestephens/util/InputReader.java
@@ -3,6 +3,9 @@ package uk.co.georgestephens.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class InputReader {
 	
@@ -10,21 +13,40 @@ public class InputReader {
 	public static String getFileAsString(String filePath) throws IOException {
 		System.out.println("Getting file: " + filePath);
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
-		StringBuilder sb = new StringBuilder();
+		StringBuilder fileContents = new StringBuilder();
 		try {
 		    String line = br.readLine();
 
 		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
+		        fileContents.append(line);
+		        fileContents.append(System.lineSeparator());
 		        line = br.readLine();
 		    }
-			return sb.toString();
+			return fileContents.toString();
 		} finally {
 		    br.close();
 		}
 	}
 	
 	//TODO 1 Make get file as ArrayList
+	public static ArrayList<String> getFileAsArray(String filePath) {
+		ArrayList<String> fileContents = new ArrayList<String>();
+	
+		// we create a scanner for reading the file
+		try (Scanner scanner = new Scanner(Paths.get(filePath))) {
+
+		    // we read the file until all lines have been read
+		    while (scanner.hasNextLine()) {
+		        // we read one line
+		        String row = scanner.nextLine();
+		        // we print the line that we read
+		        fileContents.add(row);
+		    }
+		} catch (Exception e) {
+		    System.out.println("Error: " + e.getMessage());
+		}
+		
+		return fileContents;
+	}
 
 }
